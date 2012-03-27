99aaa0f6038b97563342ca66248a6ede1fa648c1
James Chen <ashchan@gmail.com>
2012-03-27 20:32:45 +0900

    extracted aqi colors from model.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,7 +1,7 @@
 module Helpers
   def aqi_table_rows
     Matter::EPA_TABLE.map do |r|
-      %(<tr><td style="background:##{r[6]}">#{r[2]}~#{r[3]}: #{r[5]}</td></tr>)
+      %(<tr><td data-aqi="#{r[4]}">#{r[2]}~#{r[3]}: #{r[5]}</td></tr>)
     end
   end
 end
--- lib/matter.rb
+++ lib/matter.rb
@@ -18,24 +18,23 @@ class Matter
       all(:date => last.date)
     end
 
+    # return [aqi value, category in en, category in zh]
     def aqi(concentration)
       r = EPA_TABLE.detect { |l| (l[0]..l[1]).include?(concentration) }
       v = (r[3] - r[2]) / (r[1] - r[0]) * (concentration - r[0]) + r[2]
-      [v.round, r[5], r[6]]
+      [v.round, r[4], r[5]]
     end
   end
 
   # http://en.wikipedia.org/wiki/Air_quality
   EPA_TABLE = [
-    [0,     15.4,  0,   50,  "Good",      "优良",  "00e400"],
-    [15.5,  35.4,  51,  100, "Moderate",  "中等",  "ffff00"],
-    [35.5,  65.4,  101, 150, "Unhealthy for Sensitive Groups",  "敏感群体有害",  "ff7e00"],
-    #[15.5,  40.4,  51,  100, "Moderate"],
-    #[40.5,  65.4,  101, 150, "Unhealthy for Sensitive Groups"],
-    [65.5,  150.4, 151, 200, "Unhealthy",  "不健康",  "f00"],
-    [150.5, 250.4, 201, 300, "Very Unhealthy",  "非常不健康",  "99004c"],
-    [250.5, 350.4, 301, 400, "Hazardous",  "有毒害一级",  "7e0023"],
-    [350.5, 500.4, 401, 500, "Hazardous",  "有毒害二级",  "730023"]
+    [0,     15.4,  0,   50,  "Good",      "优良"],
+    [15.5,  35.4,  51,  100, "Moderate",  "中等"],
+    [35.5,  65.4,  101, 150, "Unhealthy for Sensitive Groups",  "敏感群体有害"],
+    [65.5,  150.4, 151, 200, "Unhealthy",  "不健康"],
+    [150.5, 250.4, 201, 300, "Very Unhealthy",  "非常不健康"],
+    [250.5, 350.4, 301, 400, "Hazardous",  "有毒害一级"],
+    [350.5, 500.4, 401, 500, "Hazardous",  "有毒害二级"]
   ]
 
 end
