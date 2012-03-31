d53bcdc6bf9bf6abda76675ba8a7ec3f3f8d26de
James Chen <ashchan@gmail.com>
2012-03-31 13:09:21 +0900

    more styles

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,7 +1,7 @@
 module Helpers
   def aqi_table_rows
     Matter::EPA_TABLE.map do |r|
-      %(<tr><td data-aqi="#{r[4]}">#{r[2]}~#{r[3]}: #{r[5]}</td></tr>)
+      %(<tr><td data-aqi="#{r[4]}"><span>#{r[2]}~#{r[3]}</span> #{r[5]}</td></tr>)
     end
   end
 end
